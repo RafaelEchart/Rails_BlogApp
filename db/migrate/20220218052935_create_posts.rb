f18d98424908863527b51fbba792a1bedecdabe0
Rafael Echart <rafaelechartsaltos@gmail.com>
2022-02-18 00:41:04 -0500

    Added Post Migration

---
+++ db/migrate/20220218052935_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.belongs_to :user, foreign_key: true, index: true
+      t.string :title
+      t.text :text
+      t.integer :comments_counter
+      t.integer :likes_counter
+
+      t.timestamps
+    end
+  end
+end
