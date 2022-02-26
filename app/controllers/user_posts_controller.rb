c8e2c66c7bb8c196e34b8c1ef4fd72889d4927f6
Rafael Echart <rafaelechartsaltos@gmail.com>
2022-02-25 22:05:06 -0500

    N+1 setup and solving

--- app/controllers/user_posts_controller.rb
+++ app/controllers/user_posts_controller.rb
@@ -1,7 +1,7 @@
 class UserPostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @user_posts = Post.where(user_id: params[:user_id])
+    @user_posts = Post.where(user_id: params[:user_id]).includes(:comments)
   end
 
   def show
