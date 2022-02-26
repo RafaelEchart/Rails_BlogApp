4b1616a9aa1cff42917302d208f77606f42c4bc5
Rafael Echart <rafaelechartsaltos@gmail.com>
2022-02-25 20:05:29 -0500

    Add likes testing

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Like, type: :model do
   let (:user) { User.create!(name: 'Rafael', photo: 'google.com', bio: 'FrontEnd Developer', post_counter: 0) }
   let (:post) { Post.create!(user: user, title: 'Testing', text: 'Content', comments_counter: 0, likes_counter: 0) }
 
-  it 'update likes counter for post' do
+  it 'Update like_counter when like is posted' do
     post.likes.create(user: user)
     expect(post.likes_counter).to be(1)
   end
--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,3 +1,13 @@
-# RSpec.describe Like, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+require 'rails_helper'
+
+RSpec.describe Like, type: :model do
+  describe '#update_counter' do
+    let(:user) { User.create!(name: 'Paulina', photo: 'MB', bio: 'bio', post_counter: 3) }
+    let(:post) { Post.create!(title: 'Post', user_id: user.id, text: 'Some text', comments_counter: 0, likes_counter: 1) }
+
+    it 'updates the likes counter' do
+      post.likes.create(user: user)
+      expect(post.likes_counter).to eq 1
+    end
+  end
+end
