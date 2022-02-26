90ef14c8855dd0f28133ef4e410d16980f30b201
Rafael Echart <rafaelechartsaltos@gmail.com>
2022-02-25 19:47:22 -0500

    Add comment testing

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,3 +1,13 @@
-# RSpec.describe Comment, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+require 'rails_helper'
+
+RSpec.describe Like, type: :model do
+  let (:user) { User.create!(name: 'Rafael', photo: 'google.com', bio: 'FrontEnd Developer', post_counter: 0) }
+  let (:post) { Post.create!(user: user, title: 'Testing', text: 'Content', comments_counter: 0, likes_counter: 0) }
+
+  it 'update likes counter for post' do
+    post.likes.create(user: user)
+    expect(post.likes_counter).to be(1)
+  end
+
+
+end
