0b6848c4b9c1b31473534f9d67b4b197c85bbcb6
Rafael Echart <rafaelechartsaltos@gmail.com>
2022-02-28 15:36:20 -0500

    Setup devise and signup view

--- Gemfile
+++ Gemfile
@@ -3,6 +3,9 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '3.1.0'
 
+#Gem devise for authentication
+gem 'devise'
+
 # N+1 problems
 gem 'bullet'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   root 'users#index'
   
   get '/users', to: 'users#index'
   get '/users/:user_id', to: 'users#show'
-  
+
   get '/users/:user_id/posts', to: 'user_posts#index'
   get '/users/:user_id/posts/new', to: 'user_posts#new'
   get '/users/:user_id/posts/:post_id', to: 'user_posts#show'
