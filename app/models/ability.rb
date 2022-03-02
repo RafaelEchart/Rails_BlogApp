21d5e3f27096a9e4079d24eb100697d114bf767a
Rafael Echart <rafaelechartsaltos@gmail.com>
2022-03-02 16:29:07 -0500

    Initialize abitily cancan file

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,31 +4,13 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # Define abilities for the passed in user here. For example:
-    #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
-    #
-    # The first argument to `can` is the action you are giving the user
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on.
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, :published => true
-    #
-    # See the wiki for details:
-    # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
+    
+    user ||= User.new
+    can :manage, Post, user_id: user.id # if the user is logged in can manage it's own posts
+    can :create, Comment # logged in users can also create comments
+    can :create, Like # logged in users can also create likes
+
+    return unless user.admin?
+    can :manage, :all # finally we give all remaining permissions only to the 
   end
 end
