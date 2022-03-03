cfe6c703fc84a45673efe3567d486fbab0ec0db7
Rafael Echart <rafaelechartsaltos@gmail.com>
2022-03-03 17:46:15 -0500

    Linters tests passed

--- spec/views/user_index_page_spec.rb
+++ spec/views/user_index_page_spec.rb
@@ -2,40 +2,43 @@ require 'rails_helper'
 require 'capybara/rspec'
 
 describe 'User index page', type: :feature do
-  context 'when successfull' do
-    before :each do
-      @user1 = User.new(name: 'Rafael', bio: "Rafael bio",photo: "https://avatars.githubusercontent.com/u/53021807?v=4", email: 'rafael@test.com', password: '123456')
-      @user1.save
-      user2 = User.new(name: 'Promise', bio: "Promise bio", photo: "https://avatars.githubusercontent.com/u/46839548?v=4", email: 'promise@test.com', password: '123456')
-      user2.save
-      user3 = User.new(name: 'Josue', bio: "Josue bio", photo: "https://avatars.githubusercontent.com/u/58233753?v=4", email: 'josue@test.com', password: '123456')
-      user3.save
-    end
+  before :each do
+    @user1 = User.new(name: 'Rafael', bio: 'Rafael bio',
+                      photo: 'https://avatars.githubusercontent.com/u/53021807?v=4',
+                      email: 'rafael@test.com', password: '123456')
+    @user1.save
+    user2 = User.new(name: 'Promise', bio: 'Promise bio',
+                     photo: 'https://avatars.githubusercontent.com/u/46839548?v=4',
+                     email: 'promise@test.com', password: '123456')
+    user2.save
+    user3 = User.new(name: 'Josue', bio: 'Josue bio',
+                     photo: 'https://avatars.githubusercontent.com/u/58233753?v=4',
+                     email: 'josue@test.com', password: '123456')
+    user3.save
+  end
 
-    it 'I can see the name of all users' do
-        visit 'users' 
-        expect(page).to have_content('Rafael')
-        expect(page).to have_content('Promise')
-        expect(page).to have_content('Josue')
-    end
+  it 'I can see the name of all users' do
+    visit 'users'
+    expect(page).to have_content('Rafael')
+    expect(page).to have_content('Promise')
+    expect(page).to have_content('Josue')
+  end
 
-    it 'I can see the photo(Link) of all users' do
-        visit 'users' 
-        expect(page).to have_css("img[src*='https://avatars.githubusercontent.com/u/53021807?v=4']")
-        expect(page).to have_css("img[src*='https://avatars.githubusercontent.com/u/46839548?v=4']")
-        expect(page).to have_css("img[src*='https://avatars.githubusercontent.com/u/58233753?v=4']")
-    end
-    
-    it 'I can see the number of post of all users (Everybody have 0, these are recently created users)' do
-        visit 'users' 
-        expect(page).to have_content('Post: 0')
-    end
-   
-    it 'When I click on a user button, I am redirected to that user show page.' do
-        visit 'users' 
-        click_link('button_rafael@test.com')
-        expect(current_path).to eq("/users/#{@user1.id}")
-    end
-    
+  it 'I can see the photo(Link) of all users' do
+    visit 'users'
+    expect(page).to have_css("img[src*='https://avatars.githubusercontent.com/u/53021807?v=4']")
+    expect(page).to have_css("img[src*='https://avatars.githubusercontent.com/u/46839548?v=4']")
+    expect(page).to have_css("img[src*='https://avatars.githubusercontent.com/u/58233753?v=4']")
+  end
+
+  it 'I can see the number of post of all users (Everybody have 0, these are recently created users)' do
+    visit 'users'
+    expect(page).to have_content('Post: 0')
+  end
+
+  it 'When I click on a user button, I am redirected to that user show page.' do
+    visit 'users'
+    click_link('button_rafael@test.com')
+    expect(current_path).to eq("/users/#{@user1.id}")
   end
 end
--- spec/views/user_show_page_spec.rb
+++ spec/views/user_show_page_spec.rb
@@ -2,58 +2,55 @@ require 'rails_helper'
 require 'capybara/rspec'
 
 describe 'User profile page', type: :feature do
-    before :each do
-      @user = User.create(name: 'Rafael', bio: "This is my life",photo: "https://avatars.githubusercontent.com/u/53021807?v=4", email: 'rafael@test.com', password: '123456')
-      @post1 = Post.create(user_id: @user.id, title: 'First Blog Post', text: 'This is a test 1' )
-      post2 = Post.create(user_id: @user.id, title: 'Second Blog Post', text: 'This is a test 2' )
-      post3 = Post.create(user_id: @user.id, title: 'Third Blog Post', text: 'This is a test 3' )
-    end
+  before :each do
+    @user = User.create(name: 'Rafael', bio: 'This is my life',
+                        photo: 'https://avatars.githubusercontent.com/u/53021807?v=4',
+                        email: 'rafael@test.com', password: '123456')
 
-    it 'I can see the users profile picture' do
-        visit "users/#{@user.id}"
-        expect(page).to have_css("img[src*='https://avatars.githubusercontent.com/u/53021807?v=4']")  
-    end
+    @post1 = Post.create(user_id: @user.id, title: 'First Blog Post', text: 'This is a test 1')
+    Post.create(user_id: @user.id, title: 'Second Blog Post', text: 'This is a test 2')
+    Post.create(user_id: @user.id, title: 'Third Blog Post', text: 'This is a test 3')
+  end
 
-    it 'I can see the users username' do
-        visit "users/#{@user.id}"
-        expect(page).to have_content('@Rafael')
-    end
+  it 'I can see the users profile picture' do
+    visit "users/#{@user.id}"
+    expect(page).to have_css("img[src*='https://avatars.githubusercontent.com/u/53021807?v=4']")
+  end
 
-    it 'I can see the number of posts the user has written.' do
-        visit "users/#{@user.id}"
-        expect(page).to have_selector(:id, 'profile_user_counter', text: '3')
-    end
+  it 'I can see the users username' do
+    visit "users/#{@user.id}"
+    expect(page).to have_content('@Rafael')
+  end
 
-    it 'I can see the users bio.' do
-        visit "users/#{@user.id}"
-        expect(page).to have_content('This is my life')
-    end
+  it 'I can see the number of posts the user has written.' do
+    visit "users/#{@user.id}"
+    expect(page).to have_selector(:id, 'profile_user_counter', text: '3')
+  end
 
-    it 'I can see the users first 3 posts.' do
-        visit "users/#{@user.id}"
-        expect(@user.recent_posts.length).to eq(3)
-    end
-    
-    it 'I can see a button that lets me view all of a users posts.' do
-        visit "users/#{@user.id}"
-        expect(page).to have_button('See all posts')
-    end
-   
-    it 'When I click a users post, it redirects me to that posts show page.' do
-        visit "users/#{@user.id}"
-        click_link("post_#{@post1.id}")
-        expect(current_path).to eq("/users/#{@user.id}/posts/#{@post1.id}")
-    end
-    
-    it 'When I click to see all posts, it redirects me to the users posts index page.' do
-        visit "users/#{@user.id}"
-        click_link("See all posts")
-        expect(current_path).to eq("/users/#{@user.id}/posts")
-    end
+  it 'I can see the users bio.' do
+    visit "users/#{@user.id}"
+    expect(page).to have_content('This is my life')
+  end
 
+  it 'I can see the users first 3 posts.' do
+    visit "users/#{@user.id}"
+    expect(@user.recent_posts.length).to eq(3)
+  end
 
+  it 'I can see a button that lets me view all of a users posts.' do
+    visit "users/#{@user.id}"
+    expect(page).to have_button('See all posts')
+  end
 
-   
-    
-  
+  it 'When I click a users post, it redirects me to that posts show page.' do
+    visit "users/#{@user.id}"
+    click_link("post_#{@post1.id}")
+    expect(current_path).to eq("/users/#{@user.id}/posts/#{@post1.id}")
+  end
+
+  it 'When I click to see all posts, it redirects me to the users posts index page.' do
+    visit "users/#{@user.id}"
+    click_link('See all posts')
+    expect(current_path).to eq("/users/#{@user.id}/posts")
+  end
 end
