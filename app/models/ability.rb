67518dcb484c1bac78eb482ce50daf797d8ad7fa
Rafael Echart <rafaelechartsaltos@gmail.com>
2022-03-04 21:01:06 -0500

    Auth error fixed

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,24 +2,11 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+
     user ||= User.new
-    # Define User abilities
-    # if user.is? :admin
-    #   can :manage, Post
-    # else
-    #   can :read, Post
-    # end
-    can :read, User # all users can view users
-    can :read, Post # all users can view posts
-    can :read, Comment # all users can view comments
-    return unless user.present?
+    can :manage, Post, user_id: user.id # if the user is logged in can manage it's own posts
+    can :manage, Comment, user_id: user.id
 
-    can :read, User # all users can view users
-    can :manage, Post, user_id: user.id # only post owners can manage posts
-    can :manage, Comment, user_id: user.id # only post owners can manage posts
-
-    return unless user.is? :admin
-
-    can :manage, :all
+    
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
 
+  ROLES = %w[admin user].freeze
+
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   validates :name, presence: { message: 'Name must not be blank' }
@@ -10,9 +13,9 @@ class User < ApplicationRecord
             numericality: { only_integer: true, greater_than_or_equal_to: 0,
                             message: 'Post counter should be greater or equal to 0' }
 
-  def is?(requested_role)
-    role == requested_role.to_s
-  end
+                            def is?(requested_role)
+                              role == requested_role.to_s
+                            end
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)
